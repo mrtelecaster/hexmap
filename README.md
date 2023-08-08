383f87942e3c01ecd2c86fe0cdfedfddba53b405
Fernando A. Fraticelli <53105809+mrtelecaster@users.noreply.github.com>
2023-08-08 10:33:32 -0400

    docs: add copyright/license info to documentation

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 Rust library for handling hexagon map logic and math for games. Intended for use with [the Bevy game engine](https://bevyengine.org/), but the crate itself is designed to be engine-agnostic.
 
-## Support the Project
-
 [![Static Badge](https://img.shields.io/badge/Patreon-NandoGamedev-FF424D?logo=patreon)](https://www.patreon.com/NandoGamedev)
 [![Static Badge](https://img.shields.io/badge/Ko--Fi-nando__gamedev-FF5E5B?logo=ko-fi)](https://ko-fi.com/nando_gamedev)
+
+## Copyright/License
+
+Copyright 2023 Fernando A. Fraticelli
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
-//! Hexagon tile math and logic library for game development. Based on
-//! [the article *Hexagonal Grids* by Red Blob Games](https://www.redblobgames.com/grids/hexagons/).
+//! # Hexmap
 //! 
-//! ## Support the Project
+//! Hexagon tile math and logic library for game development. Based on [the article *Hexagonal
+//! Grids* by Red Blob Games](https://www.redblobgames.com/grids/hexagons/).
+//! 
+//! [![Static Badge](https://img.shields.io/badge/Patreon-NandoGamedev-FF424D?logo=patreon)](https://www.patreon.com/NandoGamedev)
+//! [![Static Badge](https://img.shields.io/badge/Ko--Fi-nando__gamedev-FF5E5B?logo=ko-fi)](https://ko-fi.com/nando_gamedev)
+//! 
+//! ## Copyright/License
+//! 
+//! Copyright 2023 Fernando A. Fraticelli
+//! 
+//! This program is free software: you can redistribute it and/or modify it under the terms of the
+//! GNU General Public License as published by the Free Software Foundation, either version 3 of the
+//! License, or (at your option) any later version.
+//! 
+//! This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//! without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+//! the GNU General Public License for more details.
+//! 
+//! You should have received a copy of the GNU General Public License along with this program. If
+//! not, see <https://www.gnu.org/licenses/>. 
 
 
 mod coords; pub use coords::*;
