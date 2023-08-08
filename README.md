2cfb8842dfd8fcc9f47de5a9b8de2fcce476c013
Fernando A <53105809+mrtelecaster@users.noreply.github.com>
2023-08-08 11:46:24 -0400

    chore: prepare for initial release
    * bump version
    * fill in Cargo.toml package info
    * Select/define license (GLP3 or Later) and add license info to documentation
    * Remove examples (messy/redundant, better examples can be added at a later date)
    * Fill in various documentation comments

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 Rust library for handling hexagon map logic and math for games. Intended for use with [the Bevy game engine](https://bevyengine.org/), but the crate itself is designed to be engine-agnostic.
 
-## Support the Project
-
 [![Static Badge](https://img.shields.io/badge/Patreon-NandoGamedev-FF424D?logo=patreon)](https://www.patreon.com/NandoGamedev)
 [![Static Badge](https://img.shields.io/badge/Ko--Fi-nando__gamedev-FF5E5B?logo=ko-fi)](https://ko-fi.com/nando_gamedev)
+
+This library is not intended for long term use on its own - it is a test bed for concepts for a more advanced tilemapping library, as well as a foundation for me to start working on my own hexagon based games before this more advanced library is ready for use. If you can't wait to start using my code to include tilemapping in your own game, migration from this crate to the new one when it eventually becomes available should be easy, as its design is based on and informed by this crate, as well as being designed with migrating my own game over in mind. Plus, the crate isn't too complicated at the moment that it could cause major architectural problems in a game that uses it.
+
+## Copyright/License
+
+Copyright 2023 Fernando A. Fraticelli
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,41 @@
-//! Hexagon tile math and logic library for game development. Based on
-//! [the article *Hexagonal Grids* by Red Blob Games](https://www.redblobgames.com/grids/hexagons/).
+//! # Hexmap
 //! 
-//! ## Support the Project
+//! Hexagon tile math and logic library for game development. Based on [the article *Hexagonal
+//! Grids* by Red Blob Games](https://www.redblobgames.com/grids/hexagons/).
+//! 
+//! [![Static Badge](https://img.shields.io/badge/Patreon-NandoGamedev-FF424D?logo=patreon)](https://www.patreon.com/NandoGamedev)
+//! [![Static Badge](https://img.shields.io/badge/Ko--Fi-nando__gamedev-FF5E5B?logo=ko-fi)](https://ko-fi.com/nando_gamedev)
+//! 
+//! This library is not intended for long term use on its own - it is a test bed for concepts for a
+//! more advanced tilemapping library, as well as a foundation for me to start working on my own
+//! hexagon based games before this more advanced library is ready for use. If you can't wait to
+//! start using my code to include tilemapping in your own game, migration from this crate to the
+//! new one when it eventually becomes available should be easy, as its design is based on and
+//! informed by this crate, as well as being designed with migrating my own game over in mind. Plus,
+//! the crate isn't too complicated at the moment that it could cause major architectural problems
+//! in a game that uses it.
+//! 
+//! ## Copyright/License
+//! 
+//! Copyright 2023 Fernando A. Fraticelli
+//! 
+//! This program is free software: you can redistribute it and/or modify it under the terms of the
+//! GNU General Public License as published by the Free Software Foundation, either version 3 of the
+//! License, or (at your option) any later version.
+//! 
+//! This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//! without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+//! the GNU General Public License for more details.
+//! 
+//! You should have received a copy of the GNU General Public License along with this program. If
+//! not, see <https://www.gnu.org/licenses/>. 
 
 
 mod coords; pub use coords::*;
 mod map; pub use map::*;
 
 
+/// Certain algorithms require an orientation that determines how the hex grid is oriented on the X/Y plane
 #[derive(Clone, Copy, Debug)]
 pub enum Orientation
 {
@@ -35,6 +63,7 @@ impl Orientation
         }
     }
 
+	/// X-axis or horizontal spacing between tiles of a given orientation
     pub fn tile_spacing_x(&self) -> f32
     {
         match self {
@@ -43,6 +72,7 @@ impl Orientation
         }
     }
 
+	/// Y-axis or vertical spacing between tiles of a given orientation
     pub fn tile_spacing_y(&self) -> f32
     {
         match self {
@@ -51,6 +81,7 @@ impl Orientation
         }
     }
 
+	/// The corner positions of the hexagon representing a tile of a given orientation
     pub fn tile_corners(&self) -> [(f32, f32);6]
     {
         match self {
@@ -64,11 +95,8 @@ impl Orientation
 /// Constants used by the crate. Mostly used for hexagon sizes and corner positions.
 /// 
 /// <https://www.redblobgames.com/grids/hexagons/#spacing>
-pub mod constants
+mod constants
 {
-    /// Square root of 3
-    pub const SQRT_3: f32 = 1.73205080757;
-
     /// Distance between top and bottom flats
     pub const FLAT_TOP_HEIGHT: f32 = SQRT_3;
 
@@ -108,4 +136,7 @@ pub mod constants
     pub const POINTY_TOP_X_SPACING: f32 = POINTY_TOP_WIDTH;
 
     pub const POINTY_TOP_Y_SPACING: f32 = POINTY_TOP_HEIGHT / 4.0 * 3.0;
+
+	/// Square root of 3
+    const SQRT_3: f32 = 1.73205080757;
 }
